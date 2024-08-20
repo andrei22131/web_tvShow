660de62ab697fcb6b23649bc6d7e89cb3789cb95
Andrei Anton <antonandrei2003@hotmail.com>
2024-08-19 21:32:32 -0400

    router implementation

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,13 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import HomeView from "@/views/HomeView.vue";
+import DetailsView from "../views/DetailsView.vue";
+import SeasonEpisodesView from "../views/SeasonEpisodesView.vue";
+import DetailsEpisodeView from "../views/DetailsEpisodeView.vue";
+import LoginView from "../views/LoginView.vue";
+import SignUp from "../views/SignUp.vue";
+import ProfileView from "../views/ProfileView.vue";
+import JouerEpisodeView from "../views/JouerEpisodeView.vue";
+import HistoryView from "../views/HistoryView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +17,46 @@ const router = createRouter({
       name: "home",
       component: HomeView
     },
+    {
+      path: "/details/:animesId",
+      name: "DetailsView",
+      component: DetailsView
+    },
+    {
+      path: "/seasonEpisodes/:seasonId",
+      name: "SeasonEpisodesView",
+      component: SeasonEpisodesView
+    },
+    {
+      path: "/episode/:episodeId",
+      name: "DetailsEpisodeView",
+      component: DetailsEpisodeView
+    },
+    {
+      path: "/login",
+      name: "LoginView",
+      component: LoginView
+    },
+    {
+      path: "/signup",
+      name: "SignUp",
+      component: SignUp
+    },
+    {
+      path: "/profile",
+      name: "ProfileView",
+      component: ProfileView
+    },
+    {
+      path: "/jouerEpisode/:episodeId",
+      name: "JouerEpisodeView",
+      component: JouerEpisodeView
+    },
+    {
+      path: "/history",
+      name: "HistoryView",
+      component: HistoryView
+    }
   ]
 });
 
