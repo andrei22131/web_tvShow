660da2c8c7d82fa17c664efda79ea819fa0c9b80
Andrei Anton <antonandrei2003@hotmail.com>
2024-08-26 19:47:31 -0400

    Cards for the actors

---
+++ src/views/ActeursView.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="columns is-mobile scrollmenu" v-if="acteur !== null" role="list">
+        <div class="column is-one-fifth" v-for="a in acteur.roles" v-bind:key="a.roleId">
+            <div class="card">
+                <div class="card-image">
+                    <figure class="image">
+                        <img v-bind:src="a.imgURL" v-bind:alt="a.name" />
+                    </figure>
+                </div>
+                <div class="card-content">
+                    <div class="media">
+                        <div class="media-content">
+                            <p class="title is-4 no-padding has-text-centered">{{ a.name }}</p>
+                            <p class="title is-4 no-padding has-text-centered">{{ a.character }}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ActeursView",
+    props: {
+        acteur: Object,
+    },
+};
+</script>
