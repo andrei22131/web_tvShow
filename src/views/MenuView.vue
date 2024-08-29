8316a60dd2918c7ba04c6b7454c2bf4e05683efe
Andrei Anton <antonandrei2003@hotmail.com>
2024-08-29 17:24:11 -0400

    new nav bar header part.1

--- src/views/MenuView.vue
+++ src/views/MenuView.vue
@@ -1,47 +1,44 @@
 <template>
-    <nav class="navbar has-background-link" role="navigation" aria-label="main navigation">
+    <nav class="navbar has-background-dark is-spaced" role="navigation" aria-label="main navigation">
         <div id="Idnavbar" class="navbar-menu">
-            <div class="navbar-start ">
-                <router-link class="navbar-item has-text-white" v-bind:to="{ name: 'home' }">
-                    Web TV shows catalog application
+            <div class="navbar-start">
+                <router-link class="navbar-item has-text-light title is-4" v-bind:to="{ name: 'home' }">
+                    Anime Catalog
                 </router-link>
-
             </div>
         </div>
         <div class="navbar-end">
             <div class="navbar-item">
                 <div class="buttons">
-                    <router-link v-if="store.token === ''" class="navbar-item has-text-white" v-bind:to="{ name: 'SignUp' }"
-                        role="button">
+                    <router-link v-if="store.token === ''" class="button is-primary is-outlined is-rounded"
+                        v-bind:to="{ name: 'SignUp' }" role="button">
                         Sign up
                     </router-link>
-                    <router-link v-if="store.token !== ''" class="navbar-item has-text-white"
+                    <router-link v-if="store.token !== ''" class="button is-link is-light is-rounded"
                         v-bind:to="{ name: 'HistoryView' }" role="button">
                         History
                     </router-link>
-                    <router-link v-if="store.token !== ''" class="navbar-item has-text-white"
+                    <router-link v-if="store.token !== ''" class="button is-link is-light is-rounded"
                         v-bind:to="{ name: 'ProfileView' }" role="button">
                         Profile
                     </router-link>
-                    <router-link v-if="store.token === ''" class="navbar-item has-text-white"
+                    <router-link v-if="store.token === ''" class="button is-primary is-outlined is-rounded"
                         v-bind:to="{ name: 'LoginView' }" role="button">
                         Login
                     </router-link>
-                    <router-link v-if="store.token !== ''" class="navbar-item has-text-white" v-bind:to="{ name: 'home' }"
-                        v-on:click="deleteToken" role="button">
+                    <router-link v-if="store.token !== ''" class="button is-danger is-light is-rounded"
+                        v-bind:to="{ name: 'home' }" v-on:click="deleteToken" role="button">
                         Logout
                     </router-link>
-                    <router-link class="navbar-item has-text-white" v-bind:to="{ name: 'home' }" role="button">
+                    <router-link class="button is-info is-light is-rounded" v-bind:to="{ name: 'home' }" role="button">
                         About
                     </router-link>
-
-
                 </div>
             </div>
         </div>
     </nav>
 </template>
-    
+
 <script>
 import { myStore } from "@/stores/counter";
 
@@ -61,3 +58,26 @@ export default {
     },
 };
 </script>
+
+<style scoped>
+.navbar {
+    padding: 1rem 1.5rem;
+    box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+}
+
+.navbar-item {
+    transition: background-color 0.3s, color 0.3s;
+}
+
+.navbar-item:hover {
+    background-color: rgba(255, 255, 255, 0.1);
+}
+
+.buttons .button {
+    margin-right: 0.75rem;
+}
+
+.button.is-rounded {
+    border-radius: 30px;
+}
+</style>
